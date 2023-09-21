cce537ea50ba0f909edbaf241cbfcc025a9233f3
GeorgeOduor <george.wamaya@gmail.com>
2023-09-21 05:25:42 +0300

    borrower information

--- dogoloan/borrow/urls.py
+++ dogoloan/borrow/urls.py
@@ -3,5 +3,5 @@ from .views import *
 app_name = "borrow"
 
 urlpatterns = [
-    path('profile',BorrowerProfile.as_view(),name="borrower_profile"),
+    path('profile',BorrowerProfileView.as_view(),name="borrower_profile"),
 ]
--- dogoloan/borrow/views.py
+++ dogoloan/borrow/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 from django.views import View
 # Create your views here.
+from users.models import Profile
+from .models import BorrowerProfile
 
-
-class BorrowerProfile(View):
+class BorrowerProfileView(View):
+    profile  = Profile.objects
+    borrower = BorrowerProfile.objects
     def get(self,request):
+        profile = self.profile.get(user=request.user)
+        borrower = self.borrower.get(user=request.user)
         context = {
             'user_type': 'borrower',
+            'first_name': profile.first_name,
+            'last_name' : profile.last_name,
+            'mobile_no' : profile.user.msisdn,
+            'email'     : profile.email_address,
+            # physical address
+            'country'   : borrower.country,
+            'county'    : borrower.county,
+            'zip_code'  : borrower.zip_code,
+            # demographics
+            'marital_status' : borrower.marital_status,
+            'education': borrower.education,
+            'employment_status': borrower.employment_status,
+            'employment_sector': borrower.employment_sector,
+            'income_range': borrower.income_range,
+            
         }
+        
         return render(request, 'lenders/profile.html',context=context)
 
 class AvailableLenders(View):
